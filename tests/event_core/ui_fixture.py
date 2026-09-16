c74461c569528d50ed088d0a6843b27a83345409
ibaminov1-a11y <i.b.aminov1@gmail.com>
2026-09-16 11:39:00 +0500

    test: refresh all R3 market layers in Android fixture

--- tests/event_core/ui_fixture.py
+++ tests/event_core/ui_fixture.py
@@ -20,8 +20,11 @@ def reset():
         engine.config=Config(fee_per_lot=0);engine.campaign=None;engine.emergency=False;engine.recovery=False
         engine.daily_latch='';engine.auto=False;engine.paused=True;engine.exit_pending=False;engine.ack=[0,0]
         engine.account_key='';engine.history_time=0;engine.strategy=Strategy(engine.config)
-        engine.last_bars_at=0;engine.bars=[];engine.context=[];engine.rate_times=[];engine.last_exit=0;engine.save()
-        broker.bar_data=wave(int(time.time()));broker.ctx_data=wave(int(time.time()),tf=900)
+        engine.last_bars_at=0;engine.bars=[];engine.context=[];engine.m1=[];engine.m15=[];engine.h1=[];engine.live_bar=None
+        engine.rate_times=[];engine.last_exit=0;engine.last_market_attempt=-1.;engine.bar_errors=[];engine.market_errors=[];engine.save()
+        now=int(time.time())
+        broker.bar_data=wave(now);broker.ctx_data=wave(now,tf=900)
+        broker.m1_data=wave(now,tf=60,trend=.000006);broker.h1_data=wave(now,tf=3600,trend=.00008)
         engine.step()
 @app.post('/test/reset')
 def fixture_reset():reset();return jsonify(ok=True)
